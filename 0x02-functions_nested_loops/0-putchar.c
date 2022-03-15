bf694c9240ae9e6702cd6c407f176dae97083cc8
TeeGreat <toyinatofarati64@gmail.com>
2022-03-15 15:59:12 +0100

    prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include"school.h"
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	_putchar('S');
+	_putchar('c');
+	_putchar('h');
+	_putchar('o');
+	_putchar('o');
+	_putchar('l');
+	_putchar('\n');
+	return (0);
+}
