08258a42aeb0b7f847232420e6d8154afa016d30
TeeGreat <toyinatofarati64@gmail.com>
2022-03-15 17:01:40 +0100

    fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "maioin.h"
 
 /**
 * main - Entry point
@@ -7,13 +7,13 @@
 */
 int main(void)
 {
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 
